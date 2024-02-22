2cd0085db9d3743a949c328e6d531ea1a44ea833
Carol <85597024+CarolineNeris@users.noreply.github.com>
2024-02-22 20:03:26 -0300

    Revert "feat(usuario)"

--- MVCMovie/Models/User.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcMovie.Models;
-public class User
-{
-public int UserId { get; set; }
-public string Nome { get; set; }
-public string Email { get; set; }
-public  string PassWord { get; set; }
-
-}
